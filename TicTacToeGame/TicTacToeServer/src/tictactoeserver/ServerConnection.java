7e05a83a49d3a6f3a1adf31c485891f3a1d77610
ehsan <ahmednoha771@gmail.com>
2023-05-11 00:28:21 +0300

    adding setgame , getgame , deletemove , deletgame , incnumofwins

--- TicTacToeGame/TicTacToeServer/src/tictactoeserver/ServerConnection.java
+++ TicTacToeGame/TicTacToeServer/src/tictactoeserver/ServerConnection.java
@@ -14,6 +14,9 @@ import java.util.logging.Logger;
 import java.io.InputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import tictactoeclient.GameDTO;
 import tictactoeclient.Messages;
 import tictactoeclient.UserDTO;
 
@@ -30,7 +33,8 @@ public class ServerConnection {
     Socket socket;
     String ip;
     int portNum;
-    UserDTO obj;
+    UserDTO objUser;
+    GameDTO objGame;
     String msg;
 
     public ServerConnection(Socket socket) {
@@ -42,7 +46,8 @@ public class ServerConnection {
             outputstream = socket.getOutputStream();
             objectinputstream = new ObjectInputStream(inputstream);
             objectoutputstream = new ObjectOutputStream(outputstream);
-
+            
+            DataAccessLayer.connect();
             readMessage();
 
         } catch (IOException ex) {
@@ -53,6 +58,9 @@ public class ServerConnection {
     public String getIp() {
         return ip;
     }
+    
+    
+    
 
     public void readMessage() {
         new Thread() {
@@ -65,34 +73,60 @@ public class ServerConnection {
 
                         if (msg.equals(Messages.loginRequest)) {
 
-                            obj = (UserDTO) objectinputstream.readObject();
+                            objUser = (UserDTO) objectinputstream.readObject();
                             sendMessage(Messages.loginResponse, loginValidation());
 
                         } else if (msg.equals(Messages.registrationRequest)) {
 
-                            obj = (UserDTO) objectinputstream.readObject();
+                            objUser = (UserDTO) objectinputstream.readObject();
                             sendMessage(Messages.registrationResponse, registrationValidation());
 
+                        } else if (msg.equals(Messages.getOnlineUsersRequest)) {
+                            sendMessage(Messages.getOnlineUsersResponse, retriveOnlineUsers());
+                            
+
+                        } else if (msg.equals(Messages.incNumOfWinsRequest)){
+                            sendMessage(Messages.getOnlineUsersResponse, incNumOfWins());
+                            
+                        
+                        } else if (msg.equals(Messages.setGameRequest)){
+                             objGame = (GameDTO) objectinputstream.readObject();
+                              sendMessage(Messages.setGameResponse, setGameResponse());
+                              
+                        } else if (msg.equals(Messages.getGameRequest)){
+                            sendMessage(Messages.getGameResponse, retriveGame());
+                            
+                        } else if (msg.equals(Messages.deleteGameRequest)){
+                            sendMessage(Messages.deleteGameResponse, gameDeleted());
+                            
+                        }else if (msg.equals(Messages.deleteMoveRequest)){
+                            sendMessage(Messages.deleteMoveResponse, moveDeleted());
                         }
 
-                    } catch (ClassNotFoundException | IOException e) {
-                        e.printStackTrace();
+                    } catch (IOException ex) {
+                        Logger.getLogger(ServerConnection.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(ServerConnection.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (SQLException ex) {
+                        Logger.getLogger(ServerConnection.class.getName()).log(Level.SEVERE, null, ex);
                     }
+
                 }
-
             }
-        }.start();
 
+        }.start();
     }
 
-    public void sendMessage(String msg, Boolean status) {
+
+
+    public void sendMessage(String msg, Object obj) {
         new Thread() {
             @Override
             public void run() {
 
                 try {
                     objectoutputstream.writeObject(msg);
-                    objectoutputstream.writeObject(status);
+                    objectoutputstream.writeObject(obj);
                 } catch (IOException ex) {
                     Logger.getLogger(ServerConnection.class.getName()).log(Level.SEVERE, null, ex);
                 }
@@ -100,15 +134,49 @@ public class ServerConnection {
             }
         }.start();
     }
+    public ArrayList <UserDTO> retriveOnlineUsers () throws SQLException{
+        ArrayList <UserDTO> usersOnline ;
+        usersOnline = DataAccessLayer.getOnlinePlayers();
+        if (usersOnline != null)
+            return usersOnline;
+        else 
+            return (new ArrayList<UserDTO>());
 
+    }
+    public ArrayList <GameDTO> retriveGame () throws SQLException{
+        ArrayList <GameDTO> Game ;
+        Game = DataAccessLayer.getGame(objGame.getGameID());
+        if (Game != null)
+            return Game;
+        else 
+            return (new ArrayList<GameDTO>());
+
+    }
+    
+    public Boolean incNumOfWins(){
+    return true;
+    }
+    public Boolean  setGameResponse(){
+       
+      return true;  
+    }
+    public Boolean gameDeleted(){
+    return true;
+    }
+    public Boolean moveDeleted(){
+    return true;
+    }
+    
+    
     public Boolean loginValidation() {
         // check database and return true if exist and true and false if failed
         System.out.println("Login Validation Test");
-        if (obj.getUserName().equals("mohamed") && obj.getPassword().equals("1234")) {
+      /*  if (DataAccessLayer.login(obj.getUserName(), obj.getPassword())) {
             return true;
         } else {
             return false;
-        }
+        }*/
+      return true;
     }
 
     public boolean registrationValidation() {
